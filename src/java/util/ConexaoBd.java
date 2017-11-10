7ed910dd574088ba5dcb13938becc38cc6f88dd1
marcosradix <marcosradix@gmail.com>
2017-11-10 20:47:16 -0300

    adição do datasource

--- src/java/util/ConexaoBd.java
+++ src/java/util/ConexaoBd.java
@@ -8,7 +8,6 @@ package util;
 import java.sql.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import javax.sql.DataSource;
@@ -19,26 +18,19 @@ import javax.sql.DataSource;
  */
 public class ConexaoBd {
    private static Connection connection  = null;
-   private static final String usuario = "root";
-    private static final String senha = "";
    private static  boolean status =false;
-    private static final String URL = "jdbc:mysql://localhost:3306/systcc_db";
     
     public static Connection conecta(){
         try {
             status = true;
-           //Context context = new InitialContext();
-           //Context envCtx = (Context) context.lookup("java:comp/env");
-           //DataSource dataSource = (DataSource) envCtx.lookup("jdbc/sysTcc");
-            
-            
-            Class.forName("com.mysql.jdbc.Driver");
-            connection = DriverManager.getConnection(URL,usuario, senha);
+            InitialContext context = new InitialContext();
+           DataSource dataSource = (DataSource) context.lookup("jdbc/systcc");
+            connection = dataSource.getConnection();
             System.out.println("Conectado com sucesso!");
         } catch (SQLException ex) {
             Logger.getLogger(ConexaoBd.class.getName()).log(Level.SEVERE, null, ex);
             	System.out.println("Erro na copnexão ao banco de dados, erro = "+ex);
-        } catch (ClassNotFoundException ex) {
+        } catch (NamingException ex) {
            Logger.getLogger(ConexaoBd.class.getName()).log(Level.SEVERE, null, ex);
        }
        return connection;
